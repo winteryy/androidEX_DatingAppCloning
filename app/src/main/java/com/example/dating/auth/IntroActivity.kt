922e68d22faf648378661ed85d6a9a80df1200aa
Lee HuiGwang <104819764+winteryy@users.noreply.github.com>
2023-07-18 20:58:37 +0900

    Splash, Intro

---
+++ app/src/main/java/com/example/dating/auth/IntroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dating.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dating.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
